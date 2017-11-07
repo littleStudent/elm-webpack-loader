efffcefcd4f854ac9c8967e7c5d6186cc719a4f4
Thomas Sattlecker <thomas.sattlecker@gmail.com>
2017-11-07 21:39:54 +0100

    logging

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ module.exports = function() {
     console.log(files);
     console.log(options);
     var compilation = elmCompiler
-      .compileToString(['/elm/Main.elm'], options)
+      .compileToString(['./src/elm/Main.elm'], options)
       .then(function(v) {
         // console.log(v);
         runningInstances -= 1;
